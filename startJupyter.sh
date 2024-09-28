c33c8c24e7ab5d0d8ab20c314a1bac5b01228e3e
Aashish Kumar <jmphedaashish@gmail.com>
2024-09-28 13:26:42 +0530

    Update Notebook

--- startJupyter.sh
+++ startJupyter.sh
@@ -1,4 +1,5 @@
 docker run -i -t -p 8888:8888 --rm --name jupyter-notebook continuumio/anaconda3 /bin/bash -c "\
+    apt install git && git clone https://github.com/kr-aashish/SentimentAnalysis.git /opt/notebooks && \
     apt update && apt install gcc -y && \
     conda install jupyter -y --quiet && \
     mkdir -p /opt/notebooks && \
